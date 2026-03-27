aa451459c7e40a323e282053ec1558f28ab3f32f
Wannn-TZY <sulaimane627@gmail.com>
2026-03-27 08:49:46 +0700

    Penyesuaiian config

--- app/constants/Config.js
+++ app/constants/Config.js
@@ -1,8 +1,21 @@
 // constants/Config.js
 import Constants from 'expo-constants';
 
-// Ganti IP ini dengan IP lokal komputer kamu jika ingin tes di HP asli
-const LOCAL_IP = '192.168.11.144'; // IP Laptop saat ini
+// Otomatis mengambil IP dari server lokal saat ini (Metro Bundler Expr)
+const getLocalIp = () => {
+    try {
+        const debuggerHost = Constants?.expoConfig?.hostUri;
+        if (debuggerHost) {
+            return debuggerHost.split(':')[0];
+        }
+    } catch (error) {
+        // Abaikan error
+    }
+    // Fallback jika tidak terdeteksi
+    return '192.168.1.10';
+};
+
+const LOCAL_IP = getLocalIp();
 
 export const BASE_URL = __DEV__
     ? `http://${LOCAL_IP}:8000/api`
